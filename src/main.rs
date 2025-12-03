6f7bbc9d225415076ab9fc7cfdbbb1fc39619449
luckybelcik <luckybelcik@gmail.com>
2025-12-03 19:39:34 +0100

    Force x11 on linux

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[cfg(target_os = "linux")]
+use winit::platform::x11::EventLoopBuilderExtX11;
+
 use crate::client::rendering::app::App;
 
 pub mod client;
@@ -5,6 +8,10 @@ pub mod shared;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     {
+        #[cfg(target_os = "linux")]
+        let event_loop = winit::event_loop::EventLoop::builder().with_x11().build()?;
+
+        #[cfg(not(target_os = "linux"))]
         let event_loop = winit::event_loop::EventLoop::builder().build()?;
         event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
         let mut app = App::default();
