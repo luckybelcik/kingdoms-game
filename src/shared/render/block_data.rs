833eb38cf0ea4f622f365d89dac41a6992d6d176
luckybelcik <luckybelcik@gmail.com>
2025-12-15 10:39:43 +0100

    Added block_data struct

---
+++ src/shared/render/block_data.rs
@@ -0,0 +1,5 @@
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct BlockData {
+    id: u16,
+    ao: u8,
+}
--- src/shared/render/mod.rs
+++ src/shared/render/mod.rs
@@ -3,3 +3,4 @@ pub mod indirect;
 pub mod per_draw_data;
 pub mod push_constants;
 pub mod vertex;
+pub mod block_data;
